e66776e0ce52475953ef2e239f83b4ad28ee38e9
VishnuDatta6 <vishnudatta06@gmail.com>
2024-01-29 07:54:57 +0530

    direct routing fix

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
 import './App.css';
 import { BrowserRouter } from "react-router-dom";
 import Router from './components/Router';
-import { useContext, useEffect } from 'react';
 import ErrorBoundary from './components/ErrorBoundary';
-import { conText } from './components/context/centralState';
 
 function App() {
-  const {setUsername} = useContext(conText);
-
-  useEffect(()=>{
-    if(localStorage.getItem("logged")){
-      setUsername(true);
-    }
-    // eslint-disable-next-line
-  },[])
   return (
     <>
     <BrowserRouter>
--- src/components/context/centralState.js
+++ src/components/context/centralState.js
@@ -3,7 +3,7 @@ import { createContext, useState } from "react";
 export const conText = createContext();
 
 const CentralState = ({ children }) => {
-  const [username, setUsername] = useState(false);
+  const [username, setUsername] = useState(localStorage.getItem("logged") ? true : false);
   return (
     <conText.Provider value={{username, setUsername}}>
       {children}
